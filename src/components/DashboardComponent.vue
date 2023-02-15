60db4fc7846778d6bd1a43ae6f692eee125abb8a
nikolairaahauge <nira36696@edu.ucl.dk>
2023-02-15 18:27:11 +0100

    Adding basic setup for the api call now, gotta make a proxy

---
+++ src/components/DashboardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard">
+    <p>hello there</p>
+    {{apiTemp}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardComponent',
+
+  data () {
+    return {
+      apiTemp: []
+    }
+  },
+
+  async mounted () {
+    
+    await fetch('https://alarm-dev.beautycos.dk/Api/Location/GetLocations',{
+      method: 'GET',
+      mode:'cors',
+      headers: {
+        'XApiKey': 'b0shazG1DpzXOpFRq9TTHHkKZOMSosUV0Jeqnly3',
+        'Accept': 'application/json',
+    }})
+    // }
+    .then(response => response.json())
+    .then(data => this.apiTemp = data)
+  }}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/api/.*': {
+        target: 'localhost:8030',
+        changeOrigin: true,
+      },
+      '^/static/.*':{
+        target: 'localhost:8080',
+        changeOrigin: true,
+      },
+    }
+  }
+});
